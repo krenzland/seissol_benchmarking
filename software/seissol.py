1f192f5151a25080557f05c0828442248b41de03
Lukas Krenz <lukas@krenz.land>
2021-12-10 16:27:39 +0100

    Refactor seissol script slightly

--- software/seissol.py
+++ software/seissol.py
@@ -9,12 +9,7 @@ def add_options_to_job_config(job_config_base, run_config, build_config, cluster
     job_config['ranks_per_node'] = run_config['ranks_per_node']
     job_config['build_id'] = run_config['build_id']
     rpn = job_config['ranks_per_node']
-    if rpn == 1:
-        job_config['cores_per_rank'] = cluster.get_cores_per_node() - 1
-    elif rpn == 2:
-        job_config['cores_per_rank'] = cluster.get_cores_per_node() // 2 - 1
-    elif rpn == 4:
-        job_config['cores_per_rank'] = cluster.get_cores_per_node() // 4 - 1
+    job_config['cores_per_rank'] = cluster.get_cores_per_node() // rpn - 1
 
     job_config['threads_per_rank'] = cluster.get_hyperthreading_factor() * job_config['cores_per_rank']
 
